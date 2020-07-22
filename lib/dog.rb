3b48bc0a74f379b23626ef0d4606ade35dee954c
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-22 17:44:04 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog
+
+  def initialize(name, breed= "Mutt") 
+    @name = name
+    @breed = breed
+  end
+ 
+  def name=(name,breed= "Mutt") 
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+ 
+  def breed
+    @breed
+  end
+ 
+
+ 
+end 
+     
+  
+  
