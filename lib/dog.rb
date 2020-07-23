738f495185e5ae18e051a9fab2558084c4563148
Lauren Stone <lstone321@gmail.com>
2020-07-23 01:41:11 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,11 @@
-class Dog 
-  
-  def initialize(breed)
-    @breed = breed 
-  end 
-  
-  def breed=(breed)
-    @breed = breed 
-  end 
-  
-  def breed 
-    @breed 
-  end 
-end 
+class Dog
+  def initialize(name, breed= "Mutt")
+    @name = name
+    @breed = breed
+  end
+  def breed(breed)
+    @breed = breed
+
+  end
+end
+ 
